30678ef6a94c93750bf5d984b4b4e672fbb07ee7
BOSS2356 <138679535+BOSS2356@users.noreply.github.com>
2026-06-13 09:06:38 +0800

    feat: update src/views/WujieMusic.vue

---
+++ src/views/WujieMusic.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-screen flex flex-col bg-slate-50 text-slate-900">
+    <div class="flex-1 flex overflow-hidden">
+      <Sidebar />
+      <main class="flex-1 overflow-y-auto">
+        <router-view />
+      </main>
+    </div>
+    <MiniPlayer />
+  </div>
+</template>
+
+<script setup>
+import Sidebar from '../components/Sidebar.vue'
+import MiniPlayer from '../components/MiniPlayer.vue'
+</script>
